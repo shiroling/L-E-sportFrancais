bfa2bf26559d03fe4504f4fd58a08bb1418f556f
shiroling <contact.shiroling@gmail.com>
2022-12-10 11:18:15 +0100

    pas de é dans le SQL

--- dataBase/creabase.sql
+++ dataBase/creabase.sql
@@ -68,7 +68,7 @@ CREATE TABLE Tournoi(
    PRIMARY KEY(Id_Tournoi),
    FOREIGN KEY(Id_Gerant) REFERENCES Gerant(Id_Gerant),
    FOREIGN KEY(Id_Jeu) REFERENCES Jeu(Id_Jeu),
-   CONSTRAINT CK_Tournoi_Portée CHECK (Portee in ('Local','National','International')),
+   CONSTRAINT CK_Tournoi_Portee CHECK (Portee in ('Local','National','International')),
    UNIQUE(nom)
 );
 CREATE SEQUENCE Seq_Tournoi START WITH 1 INCREMENT BY 1;
